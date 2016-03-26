da2296c6100618a78ef403d0323095f1ace6f402
Radu Coman <radu.coman@endava.com>
2016-03-26 15:50:37 +0200

    adding base layer and bower dependences

--- src/main/webapp/WEB-INF/client/app/js/app.js
+++ src/main/webapp/WEB-INF/client/app/js/app.js
@@ -7,26 +7,35 @@
 
             $urlRouterProvider.otherwise('/dashboard');
             $stateProvider
+                .state('appbase',{
+                    abstract:true,
+                    templateUrl:'js/commons/views/base.html'
+                })
                 .state('dashboard', {
                     url:'/dashboard',
+                    parent:'appbase',
                     templateUrl: 'js/dashboard/views/dashboard.html',
                     controller: 'DashboardCtrl'
                 })
                 .state('users', {
                     url:'/users',
+                    parent:'appbase',
                     templateUrl: 'js/users/views/users.html',
                     controller: 'UsersCtrl'
                 })
                 .state('profiles', {
                     url:'/profiles',
+                    parent:'appbase',
                     templateUrl: 'js/profiles/views/profiles.html',
                     controller: 'ProfilesCtrl'
                 }).state('teams', {
                     url:'/teams',
+                    parent:'appbase',
                     templateUrl: 'js/teams/views/teams.html',
                     controller: 'TeamsCtrl'
                 }).state('search', {
                     url:'/search',
+                    parent:'appbase',
                     templateUrl: 'js/search/views/search.html',
                     controller: 'SearchCtrl'
                 });
