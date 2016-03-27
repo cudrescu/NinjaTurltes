be8ae2183b6c81fa03758b83c8df6154fb199c1a
vdinu <vladimir.dinu92@yahoo.com>
2016-03-27 04:41:41 +0300

    added temporary storage for team presets

--- src/main/java/com/endava/hackathon/controller/RecommendController.java
+++ src/main/java/com/endava/hackathon/controller/RecommendController.java
@@ -2,6 +2,7 @@ package com.endava.hackathon.controller;
 
 import com.endava.hackathon.dto.RecommendFilter;
 import com.endava.hackathon.dto.UserProfile;
+import com.endava.hackathon.exceptions.UserNotFoundException;
 import com.endava.hackathon.service.RecommendService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -9,7 +10,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Map;
 
 @Controller
 @RequestMapping(value = "/api/recommend")
@@ -17,8 +17,6 @@ public class RecommendController {
 
     @Autowired
     private RecommendService recommendService;
-
-    private Map<String, String> saved;
 
     @RequestMapping(method = RequestMethod.POST)
     @ResponseBody
